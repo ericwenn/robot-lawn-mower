5abc0bf5af87cda11d0eba307c7bd36edbe21003
Eric Wennerberg <ericwenn@gmail.com>
2018-02-05 15:54:09 +0100

    image split done

--- camera/image-splitter/image-splitter.py
+++ camera/image-splitter/image-splitter.py
@@ -1,17 +1,30 @@
 from PIL import Image
+SPLIT_X=400
+SPLIT_Y=400
 
-img = Image.open("color-grid.jpg")
-(imageWidth, imageHeight)=img.size
 
-gridx=400
-gridy=400
-rangex=imageWidth/gridx
-rangey=imageHeight/gridy
-print rangex*rangey
-for x in xrange(rangex):
-    for y in xrange(rangey):
-        bbox=(x*gridx, y*gridy, x*gridx+gridx, y*gridy+gridy)
-        slice_bit=img.crop(bbox)
-        slice_bit.save('out/xmap_'+str(x)+'_'+str(y)+'.png', optimize=True, bits=6)
-        print 'out/xmap_'+str(x)+'_'+str(y)+'.png'
-print imageWidth
+def split_image(image_path, split_save_path=None):
+    ext = image_path.split(".")[-1]
+    img = Image.open(image_path)
+    (imageWidth, imageHeight)=img.size
+    gridx=SPLIT_X
+    gridy=SPLIT_Y
+    rangex=imageWidth/gridx
+    rangey=imageHeight/gridy
+
+    splits = []
+
+    for x in xrange(rangex):
+        for y in xrange(rangey):
+            bbox=(x*gridx, y*gridy, x*gridx+gridx, y*gridy+gridy)
+            slice_bit=img.crop(bbox)
+
+            splits.append(slice_bit)
+
+            if not split_save_path == None:
+                slice_bit.save('{}/xmap_{}_{}.{}'.format(split_save_path, x, y, ext), optimize=True, bits=6)
+                print 'out/xmap_'+str(x)+'_'+str(y)+'.png'
+
+    return splits
+
+print split_image("color-grid.jpg", "out")
