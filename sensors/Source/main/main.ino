1de354f63be89874a8dd75781798d17ef7c91b66
Famousjasper <Famousjasper@users.noreply.github.com>
2018-02-06 16:11:24 +0100

    Changed pin layout of sensors

--- sensors/Source/main/main.ino
+++ sensors/Source/main/main.ino
@@ -1,17 +1,19 @@
 #include <lawn_mower_sensors.h>
 #include <hcsr04.h>
 
-Sensor front(1,12,13);
-Sensor front2(1,12,13);
-Sensor front3(1,12,13);
-Sensor front4(1,12,13);
-Sensor front5(1,12,13);
+//Sensor declarations
+Sensor front(1,13,14);
+Sensor frontRight(2,12,11);
+Sensor frontLeft(3,10,9);
+Sensor left(4,8,7);
+Sensor right(5,6,5);
+Sensor rear(6,4,3);
+
+//Maybe we should let all sensors on each side use the same trigger pin to save pins, for example,
+//All sensors mounted on the front can share trigger pin 13?
 
 void setup() {
   
-  // put your setup code here, to run once:
-  
-
 }
 
 void loop() {
