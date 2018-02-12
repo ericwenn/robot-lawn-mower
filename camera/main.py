c6653fb6b30514e9dc41de2734fd6a0752e91688
Lebal <filip.labe123@gmail.com>
2018-02-12 15:22:54 +0100

    first sensor

--- camera/main.py
+++ camera/main.py
@@ -1,16 +1,29 @@
-from __future__ import division
 from image_splitter import split_image
 from image_analyze import most_frequent_colour
 from plot_splits import plot_image
-from colorsys import rgb_to_hsv
+import glob
+import RPi.GPIO as GPIO
 
-
-
-
-size, splits = split_image("assets/ball-on-grass.jpeg")
-
-colors = []
-for split in splits:
-    split['color'] = most_frequent_colour(split['slice'])
-
-plot_image(size, splits, "assets/")
+#infiles = set(glob.glob("assets/*")) - set(glob.glob("assets/*.out.*"))
+#for fp in infiles:
+#    size, splits = split_image(fp, split_x=25, split_y=25)
+#    colors = []
+#    for split_x in splits:
+#        for split_y in split_x:
+#            split_y['color'] = most_frequent_colour(split_y['slice'])
+#
+#
+#    fps = fp.split(".")
+#    fps[-1] = 'out.'+fps[-1]
+#    outfile = '.'.join(fps)
+#    plot_image(size, splits, outfile)
+GPIO.setmode(GPIO.BCM)
+chan_list = (17,22,27)
+GPIO.setup(chan_list,GPIO.IN)
+while True:
+    if GPIO.input(17):
+        print "Left"
+    if GPIO.input(22):
+        print "Right"
+    if GPIO.input(27):
+        print "Middle"
