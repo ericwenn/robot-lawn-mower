dea373d033f5a38de7c99e35f5b6355cae5a89b2
Famousjasper <Famousjasper@users.noreply.github.com>
2018-02-15 17:15:24 +0100

    Removed unnecessary code and did some genral cleaning

--- sonic/main/main.ino
+++ sonic/main/main.ino
@@ -1,11 +1,8 @@
 #include <Ultrasonic.h>
 
+#define DEBUG
 
 #define MAX_DISTANCE 25
-#define FRONT_OFFSET 0
-#define LEFT_OFFSET 0
-#define RIGHT_OFFSET 0
-#define REAR_OFFSET 0
 
 #define LEFTSIG 14
 #define FRONTSIG 15
@@ -17,6 +14,7 @@
 #define TRIG_FRONT 11
 #define TRIG_RIGHT 10
 #define TIMEOUT 40000UL
+
 //Sensor declarations
 
 Ultrasonic frontLeft(TRIG_LEFT,9, TIMEOUT);
@@ -28,8 +26,6 @@ Ultrasonic frontRight(TRIG_RIGHT,7, TIMEOUT);
 //Ultrasonic rearLeft(TRIG,5, TIMEOUT);
 //Ultrasonic rearRight(TRIG,4, TIMEOUT);
 
-unsigned char signals;
-
 
 void setup() {
   pinMode(LEFTSIG, OUTPUT); //LEFTSIG
@@ -37,93 +33,69 @@ void setup() {
   pinMode(RIGHTSIG, OUTPUT); //RIGHTSIG
   pinMode(REARSIG, OUTPUT); //REARSIG
   pinMode(ERRORSIG, OUTPUT); //ERRORSIG
-  //digitalWrite(FRONTSIG, LOW);
-  signals = 0;
 
-  //Used for debug
-  Serial.begin(9600);
-  Serial.println("Setup complete");
-  
+  #ifdef DEBUG
+    Serial.begin(9600);
+  #endif
 }
 
 void loop() {
-  signals = read_sensors(signals);
-  //update_signals(signals);
-
+  check_sensors();
 }
 
-unsigned char read_sensors(char c){
+void check_sensors(){
   
-  //Check front for hit, if hit _front = 1 
-  unsigned char _frontFront = frontFront.distanceRead();
-  delay(4);
-  //Check on the left and right side on the front
+  //Read sensors and trigger relevant signal pin if blocked
+  //FRONT
   unsigned char _frontLeft = frontLeft.distanceRead();
   delay(4);
-  unsigned char _frontRight = frontRight.distanceRead();
+  unsigned char _frontFront = frontFront.distanceRead();
   delay(4);
-  //De-bug
-/*
-  Serial.print(" Dist FrontRight: ");
-  Serial.print(frontRight.distanceRead());
-
-  Serial.print(" Dist Front: ");
-  Serial.print(frontFront.distanceRead());
-
-  Serial.print(" Dist FrontLeft: ");
-  Serial.println(frontLeft.distanceRead());
-  */
-
-  Serial.print("FrontLeft ");
-  Serial.print(_frontLeft);
-  Serial.print(" ");
-  Serial.print(determine_blocked(_frontLeft));
+  unsigned char _frontRight = frontRight.distanceRead();
+  digitalWrite(FRONTSIG, (determine_blocked(_frontFront) | determine_blocked(_frontRight) | determine_blocked(_frontLeft)));
+  delay(4);
   
-  if(determine_blocked(_frontLeft) == 1){ //can be better looking
-    digitalWrite(LEFTSIG,HIGH);
-    }else{
-     digitalWrite(LEFTSIG,LOW); 
-  }
-
-
-  Serial.print(" FrontFront ");
-  Serial.print(_frontFront);
-  Serial.print(" ");
-  Serial.print(determine_blocked(_frontFront));
-  if(determine_blocked(_frontFront) == 1){ //can be better looking
-    digitalWrite(FRONTSIG,HIGH);
-    }else{
-     digitalWrite(FRONTSIG,LOW); 
-  }
-
-  Serial.print(" FrontRight ");
-  Serial.print(_frontRight);
-  Serial.print(" ");
-  Serial.println(determine_blocked(_frontRight));
-  if(determine_blocked(_frontRight) == 1){ //can be better looking
-    digitalWrite(RIGHTSIG,HIGH);
-    }else{
-     digitalWrite(RIGHTSIG,LOW); 
-  }
+  //LEFT
+  //RIGHT
+  //REAR
   
-  return c;
+  #ifdef DEBUG
+    //If you use a smart serial monitor (like putty) these two lines will do you good
+    Serial.print("\033[2j");
+    Serial.print("\033[H");
+    
+    Serial.print("FrontLeft ");
+    Serial.print(_frontLeft);
+    Serial.print(" ");
+    Serial.println(determine_blocked(_frontFront));
+  
+    Serial.print("FrontFront ");
+    Serial.print(_frontFront);
+    Serial.print(" ");
+    Serial.println(determine_blocked(_frontRight));
+  
+    Serial.print("FrontRight ");
+    Serial.print(_frontRight);
+    Serial.print(" ");
+    Serial.println(determine_blocked(_frontLeft));
+    Serial.println("");
+
+    Serial.print("FRONTSIG: ");
+    Serial.println(digitalRead(FRONTSIG));
+    Serial.print("LEFTSIG: ");
+    Serial.println(digitalRead(LEFTSIG));
+    Serial.print("RIGHTSIG: ");
+    Serial.println(digitalRead(RIGHTSIG));
+    Serial.print("REARSIG: ");
+    Serial.println(digitalRead(REARSIG));
+    Serial.print("ERRORSIG: ");
+    Serial.println(digitalRead(ERRORSIG));
+  #endif
   
 }
 
-
-unsigned char determine_blocked(char dist){
-  //unsigned char dist = sensor.distanceRead();
-  //if 0, means its too far away for the sensors
-  if(dist <= MAX_DISTANCE && dist > 0){
-    return 1; 
-  }
-  return 0;
-}
-
-void update_signals(char c){
-  digitalWrite(FRONTSIG, (c & 0x00000001));
-  digitalWrite(LEFTSIG, ((c & 0x00000010)>>1));
-  digitalWrite(RIGHTSIG, ((c & 0x00000100)>>2));
-  digitalWrite(REARSIG, ((c & 0x00001000)>>3));
+//Updates given signal in case given distance is to short
+unsigned char determine_blocked(unsigned char dist){
+  return (dist <= MAX_DISTANCE);
 }
 
