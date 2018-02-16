0f2a6e9938a6e75e442769862c7f58f25510a752
Eric Wennerberg <ericwenn@gmail.com>
2018-02-16 14:39:53 +0100

    take pictures in thread

--- camera/webserver/main.py
+++ camera/webserver/main.py
@@ -4,8 +4,20 @@ from os import curdir, sep, path
 import glob
 import subprocess
 import datetime
+import threading
 
 PORT_NUMBER = 8080
+
+def worker():
+    """thread worker function"""
+    while True:
+        cmd = "raspistill -w 910 -h 700 -vf -t 1 -q 20 -o %s/pictures/%s.jpg" % (path.abspath(path.dirname(__file__)), datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S"))
+        print cmd
+        subprocess.Popen(cmd.split())
+        sleep(1)
+    return 
+t = threading.Thread(target=worker)
+t.start()
 
 #This class will handles any incoming request from
 #the browser
@@ -22,10 +34,7 @@ class myHandler(BaseHTTPRequestHandler):
         html += '</body></html>';
         return html
 
-    def take_picture(self):
-        cmd = "raspistill -w 910 -h 700 -vf -q 20 -o %s/pictures/%s.jpg" % (path.abspath(path.dirname(__file__)), datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S"))
-        print cmd
-        subprocess.Popen(cmd.split())
+
 
 
 	#Handler for the GET requests
@@ -37,13 +46,6 @@ class myHandler(BaseHTTPRequestHandler):
             self.wfile.write(self.index())
             return
 
-        if self.path=='/shoot':
-            self.take_picture()
-            self.send_response(200)
-            self.send_header('Content-type', 'text/html')
-            self.end_headers()
-            self.wfile.write('ok')
-            return
 
         try:
         #Check the file extension required and
