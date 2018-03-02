cdb2623831f0b3242fd18e87b272b84d08164306
Lebal <filip.labe123@gmail.com>
2018-03-02 10:00:30 +0100

    Checked if inputs work, they do

--- camera/steering/drive.py
+++ camera/steering/drive.py
@@ -4,15 +4,20 @@ import steer
 steer.setup()
 
 while True:
-    user_input = raw_input("Direction")
+    user_input = raw_input("Direction: ")
 
     if(user_input =="r"):
         steer.right()
-    else if(user_input == "l"):
+        #print("r")
+    elif(user_input == "l"):
         steer.left()
-    else if(user_input == "f"):
+        #print("l")
+    elif(user_input == "f"):
         steer.forward()
-    else if(user_input == "b"):
+        #print("f")
+    elif(user_input == "b"):
         steer.back()
-    else if(user_input == "s"):
+        #print("b")
+    elif(user_input == "s"):
         steer.stop()
+        #print("s")
--- camera/steering/steer.py
+++ camera/steering/steer.py
@@ -4,7 +4,6 @@ def stop():
     chan_list = (5,6,26)
     GPIO.output(chan_list,(GPIO.LOW,GPIO.LOW,GPIO.LOW))
 
-
 def right():
     chan_list = (5,6,26)
     GPIO.output(chan_list,(GPIO.LOW,GPIO.LOW,GPIO.HIGH))
