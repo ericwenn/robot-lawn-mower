268c9ca73a45465cb3319c2dfebe21a47a7f9922
Lebal <filip.labe123@gmail.com>
2018-03-05 14:28:02 +0100

    try catch

--- us-sensing/soundsensor/soundsensor.py
+++ us-sensing/soundsensor/soundsensor.py
@@ -1,20 +1,6 @@
 import RPi.GPIO as GPIO
 import time
 
-#infiles = set(glob.glob("assets/*")) - set(glob.glob("assets/*.out.*"))
-#for fp in infiles:
-#    size, splits = split_image(fp, split_x=25, split_y=25)
-#    colors = []
-#    for split_x in splits:
-#        for split_y in split_x:
-#            split_y['color'] = most_frequent_colour(split_y['slice'])
-#
-#
-#    fps = fp.split(".")
-#    fps[-1] = 'out.'+fps[-1]
-#    outfile = '.'.join(fps)
-#    plot_image(size, splits, outfile)
-
 
 let set = False
 GPIO.setmode(GPIO.BCM)
