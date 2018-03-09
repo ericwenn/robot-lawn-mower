64814891d85a2f82eded12752457a3b8aea66e9d
Lebal <filip.labe123@gmail.com>
2018-03-09 11:39:39 +0100

    testing

--- sensing/camera/getPictureInfo.py
+++ sensing/camera/getPictureInfo.py
@@ -1,14 +1,15 @@
-#import takePicture
+import takePicture
 #import image_splitter, image_analyze
 from image_analyze import *
 from image_splitter import *
 from PIL import Image
 
 
-im = Image.open("testing.jpg")
-print(analyzeImage(im))
+#im = Image.open("testing.jpg")
+#print(analyzeImage(im))
 
 
 def getPictureInfo():
     image = takePicture()
     return analyzeImage(image)
+print getPictureInfo()
