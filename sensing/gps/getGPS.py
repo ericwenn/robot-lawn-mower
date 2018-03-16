96afe2c2240e853009d7738223dda9a0c5b86d4f
Lebal <filip.labe123@gmail.com>
2018-03-16 13:45:41 +0100

    testing gps

--- sensing/gps/getGPS.py
+++ sensing/gps/getGPS.py
@@ -1,7 +1,7 @@
 #! /usr/bin/python
 # Written by Dan Mandle http://dan.mandle.me September 2012
 # License: GPL 2.0
-
+'''
 import os
 from gps import *
 from time import *
@@ -72,3 +72,35 @@ if __name__ == '__main__':
     gpsp.running = False
     gpsp.join() # wait for the thread to finish what it's doing
   print "Done.\nExiting."
+  '''
+import threading
+import time
+from gps import *
+
+class GpsPoller(threading.Thread):
+
+   def __init__(self):
+       threading.Thread.__init__(self)
+       self.session = gps(mode=WATCH_ENABLE)
+       self.current_value = None
+
+   def get_current_value(self):
+       return self.current_value
+
+   def run(self):
+       try:
+            while True:
+                self.current_value = self.session.next()
+                time.sleep(0.2) # tune this, you might not get values that quickly
+       except StopIteration:
+            pass
+
+if __name__ == '__main__':
+
+   gpsp = GpsPoller()
+   gpsp.start()
+   # gpsp now polls every .2 seconds for new data, storing it in self.current_value
+   while 1:
+       # In the main thread, every 5 seconds print the current value
+       time.sleep(5)
+       print gpsp.get_current_value()
