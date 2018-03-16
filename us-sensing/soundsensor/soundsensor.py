e7bdd7dadb964070b43b129d287135540800c714
Lebal <filip.labe123@gmail.com>
2018-03-16 11:25:07 +0100

    refractor

--- us-sensing/soundsensor/soundsensor.py
+++ us-sensing/soundsensor/soundsensor.py
@@ -1,24 +1,12 @@
 import RPi.GPIO as GPIO
 import time
 
-
 let set = False
-GPIO.setmode(GPIO.BCM)
-chan_list = (17,22,27)
-GPIO.setup(chan_list,GPIO.IN)
-while True:
-    print "Sensing"
-    if GPIO.input(17):
-        print "Left"
-    if GPIO.input(22):
-        print "Right"
-    if GPIO.input(27):
-        print "Middle"
-    time.sleep(0.5)
 
 def sensorsLMR():
     if(not(set)):
         setupGPIO
+        set = True
     pos = (False,False,False)
     if GPIO.input(17):
         pos[0]=True
@@ -33,3 +21,15 @@ def setupGPIO():
     GPIO.setmode(GPIO.BCM)
     chan_list = (17,22,27)
     GPIO.setup(chan_list,GPIO.IN)
+
+
+setupGPIO()
+while True:
+    print "Sensing"
+    if GPIO.input(17):
+        print "Left"
+    if GPIO.input(22):
+        print "Right"
+    if GPIO.input(27):
+        print "Middle"
+    time.sleep(0.5)
