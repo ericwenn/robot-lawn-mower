9745982705ebfc1756f26a5fff2e41d3ec2db683
Alex <gusnital@student.gu.se>
2018-03-20 13:28:32 +0100

    Updated ultrasensors, it works by serial conection now

--- sonic/main/main.ino
+++ sonic/main/main.ino
@@ -1,5 +1,5 @@
+#include <SoftwareSerial.h>
 #include <Ultrasonic.h>
-
 #define DEBUG
 
 #define MAX_DISTANCE 25
@@ -7,32 +7,30 @@
 #define LEFTSIG 14
 #define FRONTSIG 15
 #define RIGHTSIG 16
-#define REARSIG 17
-#define ERRORSIG 18
 
-#define TRIG_LEFT 12
-#define TRIG_FRONT 11
 #define TRIG_RIGHT 10
+#define TRIG_FRONT 11
+#define TRIG_LEFT 12
+
+#define ECHO_RIGHT 7
+#define ECHO_FRONT 8
+#define ECHO_LEFT 9
+
 #define TIMEOUT 40000UL
 
 //Sensor declarations
+Ultrasonic sonicRight(TRIG_RIGHT,ECHO_RIGHT, TIMEOUT);
+Ultrasonic sonicFront(TRIG_FRONT,ECHO_FRONT,TIMEOUT);
+Ultrasonic sonicLeft(TRIG_LEFT,ECHO_LEFT, TIMEOUT);
 
-Ultrasonic frontLeft(TRIG_LEFT,9, TIMEOUT);
-Ultrasonic frontFront(TRIG_FRONT,8,TIMEOUT);;
-Ultrasonic frontRight(TRIG_RIGHT,7, TIMEOUT);
-//Ultrasonic left(TRIG,8, TIMEOUT);
-//Ultrasonic right(TRIG,7, TIMEOUT);
-//Ultrasonic rear(TRIG,6, TIMEOUT);
-//Ultrasonic rearLeft(TRIG,5, TIMEOUT);
-//Ultrasonic rearRight(TRIG,4, TIMEOUT);
-
+SoftwareSerial serialRPi (15,16);
 
 void setup() {
   pinMode(LEFTSIG, OUTPUT); //LEFTSIG
   pinMode(FRONTSIG, OUTPUT); //FRONTSIG
   pinMode(RIGHTSIG, OUTPUT); //RIGHTSIG
-  pinMode(REARSIG, OUTPUT); //REARSIG
-  pinMode(ERRORSIG, OUTPUT); //ERRORSIG
+
+  serialRPi.begin(9600);
 
   #ifdef DEBUG
     Serial.begin(9600);
@@ -46,56 +44,61 @@ void loop() {
 void check_sensors(){
   
   //Read sensors and trigger relevant signal pin if blocked
-  //FRONT
-  unsigned char _frontLeft = frontLeft.distanceRead();
-  delay(4);
-  unsigned char _frontFront = frontFront.distanceRead();
-  delay(4);
-  unsigned char _frontRight = frontRight.distanceRead();
-  digitalWrite(FRONTSIG, (determine_blocked(_frontFront) | determine_blocked(_frontRight) | determine_blocked(_frontLeft)));
-  delay(4);
-  
   //LEFT
-  //RIGHT
-  //REAR
+  unsigned char _sonicLeft = sonicLeft.distanceRead();
+  delay(6);
+  unsigned char blockedLeft = determine_blocked(_sonicLeft);
+  digitalWrite(LEFTSIG, blockedLeft);
   
+  //FRONT
+  unsigned char _sonicFront = sonicFront.distanceRead();
+  delay(6);
+  unsigned char blockedFront = determine_blocked(_sonicFront);
+  digitalWrite(FRONTSIG,blockedFront);
+ 
+  //RIGHT
+  unsigned char _sonicRight = sonicRight.distanceRead();
+  delay(6);
+  unsigned char blockedRight = determine_blocked(_sonicRight);
+  digitalWrite(RIGHTSIG,blockedRight);
+
+  char serialValues[] = {blockedLeft,blockedFront,blockedRight};
+  serialRPi.write(serialValues);
+
   #ifdef DEBUG
     //If you use a smart serial monitor (like putty) these two lines will do you good
-    Serial.print("\033[2j");
-    Serial.print("\033[H");
+    //Serial.print("\033[2j");
+    //Serial.print("\033[H");
     
-    Serial.print("FrontLeft ");
-    Serial.print(_frontLeft);
+    Serial.print("Left ");
+    Serial.print(_sonicLeft);
     Serial.print(" ");
-    Serial.println(determine_blocked(_frontFront));
+    Serial.println(determine_blocked(_sonicLeft));
   
-    Serial.print("FrontFront ");
-    Serial.print(_frontFront);
+    Serial.print("Front ");
+    Serial.print(_sonicFront);
     Serial.print(" ");
-    Serial.println(determine_blocked(_frontRight));
+    Serial.println(determine_blocked(_sonicFront));
   
-    Serial.print("FrontRight ");
-    Serial.print(_frontRight);
+    Serial.print("Right ");
+    Serial.print(_sonicRight);
     Serial.print(" ");
-    Serial.println(determine_blocked(_frontLeft));
+    Serial.println(determine_blocked(_sonicRight));
     Serial.println("");
 
-    Serial.print("FRONTSIG: ");
+    /*Serial.print("FRONTSIG: ");
     Serial.println(digitalRead(FRONTSIG));
     Serial.print("LEFTSIG: ");
     Serial.println(digitalRead(LEFTSIG));
     Serial.print("RIGHTSIG: ");
     Serial.println(digitalRead(RIGHTSIG));
-    Serial.print("REARSIG: ");
-    Serial.println(digitalRead(REARSIG));
-    Serial.print("ERRORSIG: ");
-    Serial.println(digitalRead(ERRORSIG));
+    */
   #endif
   
 }
 
 //Updates given signal in case given distance is to short
 unsigned char determine_blocked(unsigned char dist){
-  return (dist <= MAX_DISTANCE);
+  return (dist <= MAX_DISTANCE && dist !=0);
 }
 
