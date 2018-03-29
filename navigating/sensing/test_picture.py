1014e219c617d3d5e56df1398b7f968e6e0520c7
filip.labe123@gmail.com <filip.labe123@gmail.com>
2018-03-29 11:35:17 +0000

    testing

--- navigating/sensing/test_picture.py
+++ navigating/sensing/test_picture.py
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
-from takePicture import * as camera
 
-cm = camera.camera()
-
-print cm.analyzeImage(cm.takePicture())
-=======
 from takePicture import camera as c
 
 cm = c.camera()
 print (cm.get_picture_info())
->>>>>>> origin/master
