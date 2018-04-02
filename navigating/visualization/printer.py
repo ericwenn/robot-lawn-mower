90ab191e21693e2cea671c41093682cb863725bb
Eric Wennerberg <ericwenn@gmail.com>
2018-04-02 14:38:09 +0200

    No asci

--- navigating/visualization/printer.py
+++ navigating/visualization/printer.py
@@ -1,11 +1,11 @@
-import curses
+# import curses
 
-stdscr = curses.initscr()
-curses.start_color()
-curses.init_pair(1, curses.COLOR_RED, curses.COLOR_BLACK)
+# stdscr = curses.initscr()
+# curses.start_color()
+# curses.init_pair(1, curses.COLOR_RED, curses.COLOR_BLACK)
 
 
-win = curses.newwin(5, 50, 1, 1)
+# win = curses.newwin(5, 50, 1, 1)
 
 
 
@@ -17,6 +17,6 @@ def print_ultrasound(us_reading):
   for reading in raw_data:
     graphs[0].append( '|' if reading['can_move'][0] else '.')
 
-  
-  win.addstr("".join(graphs[0]), curses.color_pair(1))
-  win.refresh()
+  print "".join(graphs[0])
+  #win.addstr("".join(graphs[0]), curses.color_pair(1))
+  #win.refresh()
