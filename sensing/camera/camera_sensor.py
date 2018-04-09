3d8f84a7b2b04d1529355a390545b887bbe6c0d4
Eric Wennerberg <ericwenn@gmail.com>
2018-04-09 14:03:20 +0200

    Init script camera sensor

--- sensing/camera/camera_sensor.py
+++ sensing/camera/camera_sensor.py
@@ -49,3 +49,13 @@ class CameraSensor(object):
             pass
 
         return self.stack[-n:]
+
+
+
+if __name__ === "__main__":
+    cam_sense = CameraSensor()
+
+    cam_sense.start()
+    while True:
+        print cam_sense.get_camera_events(3)
+        time.sleep(.5)
