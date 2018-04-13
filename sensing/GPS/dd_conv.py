ae18ec75cf584018216934142c4989fd6289f82e
Famousjasper <Famousjasper@users.noreply.github.com>
2018-04-13 15:52:40 +0200

    Added getDDconv function

--- sensing/GPS/dd_conv.py
+++ sensing/GPS/dd_conv.py
@@ -2,39 +2,41 @@ import serial
 
 ser = serial.Serial('/dev/serial0', 9600, timeout=0.5)
 
-while(True):
-    try:
-        str=ser.readline()
-    except:
-        pass
-    if(str[1:6]!="GPRMC"):
-        continue
-
-
-    #sentence to decimal degrees convertion code
-
-    data = str.split(",")
-
-    lat=data[3]
-    lat_sign=data[4]
-    lng=data[5]
-    lng_sign=data[6]
-
-    if(lat=="" or lat_sign=="" or lng=="" or lng_sign==""):
-            print("NO FIX!")
+getDDconv():
+    while True:
+        try:
+            str=ser.readline()
+        except:
+            pass
+        if(str[1:6]!="GPRMC"):
             continue
 
-    if(lat_sign == "N"):
-        lat_sign = 1
-    else:
-        lat_sign = -1
 
-    if(lng_sign == "E"):
-        lng_sign = 1
-    else:
-        lng_sign = -1
+        #sentence to decimal degrees convertion code
 
-    dd=[lat_sign*(float(lat[0:2])+(float(lat[2:])/60)),lng_sign*(float(lng[0:3])+(float(lng[3:]))/60)]
-    print(dd)
+        data = str.split(",")
 
+        lat=data[3]
+        lat_sign=data[4]
+        lng=data[5]
+        lng_sign=data[6]
 
+        if(lat=="" or lat_sign=="" or lng=="" or lng_sign==""):
+                print("NO FIX!")
+                return 1
+
+        if(lat_sign == "N"):
+            lat_sign = 1
+        else:
+            lat_sign = -1
+
+        if(lng_sign == "E"):
+            lng_sign = 1
+        else:
+            lng_sign = -1
+
+        dd=[lat_sign*(float(lat[0:2])+(float(lat[2:])/60)),lng_sign*(float(lng[0:3])+(float(lng[3:]))/60)]
+        return(dd)
+
+while True:
+    print(getDDconv())
