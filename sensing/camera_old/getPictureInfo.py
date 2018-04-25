11027ef20133a9fb6bfe3eeaab21c51f104d34e1
Eric Wennerberg <ericwenn@gmail.com>
2018-04-25 13:46:48 +0200

    Remove old files

--- sensing/camera_old/getPictureInfo.py
+++
@@ -1,9 +0,0 @@
-from takePicture import *
-from image_analyze import *
-from PIL import Image
-
-def getPictureInfo():
-    image = takePicture()
-    image.save("testing.jpeg",optimize=True,bits=6)
-    return analyzeImage(image)
-print getPictureInfo()
