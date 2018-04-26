631659d6f4b5f4c49266593156c755ffe0a34f18
Famousjasper <Famousjasper@users.noreply.github.com>
2018-04-26 11:50:45 +0200

    Changed more stuff

--- sensing/GPS/command_listener.py
+++ sensing/GPS/command_listener.py
@@ -5,13 +5,20 @@ from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 import json
 
 
-def make_handler(queue):
+def make_handler(queue, probe_Q):
     class Webserver(BaseHTTPRequestHandler):
         def do_POST(self):
             
             command = self.path[1:]
             queue.put(command)
-            self.send_response(200)
+            if(command == "probe"):
+                resp = probe_Q.get()
+                self.send_response(200)
+                self.send_header('Content-Type', 'application/json')
+                self.end_headers()
+                self.wfile.write(resp)
+            else:
+                self.send_response(200)
         
         def log_message(self, format, *args):
             return
@@ -20,20 +27,21 @@ def make_handler(queue):
 
 
 class WebserverThread(Thread):
-    def __init__(self, queue, port):
+    def __init__(self, queue, probe_Q, port):
         Thread.__init__(self)
         self.queue = queue
         self.port = port
+        self.probe_Q = probe_Q
 
     def run(self):
         server_address = ('', self.port)
-        httpd = HTTPServer(server_address, make_handler(self.queue))
+        httpd = HTTPServer(server_address, make_handler(self.queue,self.probe_Q))
         httpd.serve_forever()
 
 
 class CommandListener(object):
-    def __init__(self, port=8080, queue=None):
-        self.thread = WebserverThread(queue, port)
+    def __init__(self, port=8080, queue=None, probe_Q=None):
+        self.thread = WebserverThread(queue, probe_Q, port)
         self.isStarted = False
 
     def start(self):
