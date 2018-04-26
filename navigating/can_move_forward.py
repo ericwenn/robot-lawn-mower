537715eb113d9f102f39c95bd61870e90215d506
Eric Wennerberg <ericwenn@gmail.com>
2018-04-26 15:34:50 +0200

    debug

--- navigating/can_move_forward.py
+++ navigating/can_move_forward.py
@@ -2,7 +2,5 @@ def can_move_forward(ultrasound, camera, gps):
 
   us_verdict = ultrasound.can_move_forward()
   cam_verdict = camera.can_move_forward()
-  if us_verdict > 0 and us_verdict < 0.75 and cam_verdict < -.7:
-    return False, (us_verdict, cam_verdict)
-
-  return True, (us_verdict, cam_verdict)
+  m = min([us_verdict, cam_verdict])
+  return m > 0.7, (us_verdict, cam_verdict)
