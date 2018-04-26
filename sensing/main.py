1a5b97ef3683589a4a735f82c14ee26e171e03b1
Eric Wennerberg <ericwenn@gmail.com>
2018-04-26 14:31:05 +0200

    init for camera and gps

--- sensing/main.py
+++ sensing/main.py
@@ -1,6 +1,8 @@
 from GPS.booter import start as gps_start
 from camera.camera_sensor import start as camera_start
-
+from time import sleep
 if __name__ == '__main__':
   gps_start()
   camera_start()
+  while True:
+    sleep(5)
